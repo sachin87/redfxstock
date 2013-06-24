b0950080e2db65a3d0d13098611cb9cd488497fb
Sachin Singh <sachin.y87@gmail.com>
2013-06-25 00:15:32 +0530

    fog.rb modified

--- config/initializers/fog.rb
+++ config/initializers/fog.rb
@@ -1,8 +1,8 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
       :provider               => 'AWS',                        # required
-      :access_key_id     => ENV['S3_KEY'],
-      :secret_access_key => ENV['S3_SECRET']
+      :aws_access_key_id     => ENV['S3_KEY'],
+      :aws_secret_access_key => ENV['S3_SECRET']
   }
   config.fog_directory  = ENV['S3_BUCKET_NAME']                     # required
   config.fog_public     = false                                   # optional, defaults to true
