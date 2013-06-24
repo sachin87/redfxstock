8bced7b8e8f1ac82050d6cd59b61c37de3900389
Sachin Singh <sachin.y87@gmail.com>
2013-06-24 23:32:29 +0530

    added env variables

--- config/initializers/fog.rb
+++ config/initializers/fog.rb
@@ -1,10 +1,10 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
       :provider               => 'AWS',                        # required
-     # :aws_access_key_id      => '',                        # required
-     # :aws_secret_access_key  => '',                        # required
+      :access_key_id     => ENV['S3_KEY'],
+      :secret_access_key => ENV['S3_SECRET']
   }
-  #config.fog_directory  = ''                     # required
+  config.fog_directory  = ENV['S3_BUCKET_NAME']                     # required
   config.fog_public     = false                                   # optional, defaults to true
   config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
 end
