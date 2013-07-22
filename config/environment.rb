1c0103b978ca8dc87ea19153b3a6679539f6fbd0
Sachin87 <sachin.y87@gmail.com>
2013-07-22 23:01:16 +0530

    smtp gmail settings added

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,13 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Redfxstock::Application.initialize!
+
+config.action_mailer.delivery_method = :smtp
+config.action_mailer.smtp_settings = {
+    :address              => "smtp.gmail.com",
+    :port                 => 587,
+    :domain               => 'gmail.com',
+    :user_name            => ENV['EMAIL'],
+    :password             => ENV['PASSWORD'],
+    :authentication       => 'plain'
+}
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -36,4 +36,6 @@ Redfxstock::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
+
+  config.action_mailer.default_url_options = { :host => 'localhost' }
 end
