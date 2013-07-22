a91e6083f7494b785082b3eeddea19d45dd2af32
Sachin87 <sachin.y87@gmail.com>
2013-07-22 23:05:47 +0530

    bug fixed

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,4 +66,14 @@ Redfxstock::Application.configure do
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
 
   config.action_mailer.default_url_options = { :host => 'redfxstock.herokuapp.com' }
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+      :address              => "smtp.gmail.com",
+      :port                 => 587,
+      :domain               => 'gmail.com',
+      :user_name            => ENV['EMAIL'],
+      :password             => ENV['PASSWORD'],
+      :authentication       => 'plain'
+  }
 end
