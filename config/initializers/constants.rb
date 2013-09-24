2c8b971c7a1d1c46f9f7e3fba9a1b8d88594204c
Sachin Singh <singhsachin87@yahoo.com>
2013-09-24 18:20:34 +0530

    sidebar links partial re-factored

---
+++ config/initializers/constants.rb
@@ -0,0 +1,2 @@
+ADMIN_SECTIONS = ['Categories', 'Expertises', 'Galleries', 'Profiles', 'Roles', 'Uploads', 'Users']
+USER_SECTIONS = ['Categories', 'Expertises', 'Galleries', 'Profiles','Uploads', 'Users']
