6d34294f3fb8bd1ca6205e4497331216b5c354b2
Sachin Singh <singhsachin87@yahoo.com>
2014-02-12 15:43:17 +0100

    more view fixes, user_id added to profiles

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140212113349) do
+ActiveRecord::Schema.define(:version => 20140212134726) do
 
   create_table "categories", :force => true do |t|
     t.string   "name"
@@ -63,6 +63,7 @@ ActiveRecord::Schema.define(:version => 20140212113349) do
     t.date     "dob"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.integer  "user_id"
   end
 
   add_index "profiles", ["dob"], :name => "index_profiles_on_dob"
